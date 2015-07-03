7901e738aa50074d565966fa40d82acf5aa2e1a4
Dominic May <jack.thatch@gmail.com>
2015-07-03 15:26:28 +0800

    Some architecture (kinda)

--- main.py
+++ main.py
@@ -1,1 +1,149 @@
-ECHO is on.
+import sys
+from os.path import expanduser
+sys.path.insert(0, expanduser('~/Dropbox/temp/arcrest'))
+
+import os
+import types
+import logging
+import pkgutil
+import importlib
+from os.path import join, dirname, exists, basename
+logging.basicConfig(level=logging.DEBUG)
+
+import sections.aus_map
+
+from betamax import Betamax
+
+HERE = dirname(__file__)
+CACHE = 'd:\\stats_data\\cache'
+
+
+with Betamax.configure() as conf:
+    conf.cassette_library_dir = join(CACHE, 'cassettes')
+
+
+def dir_for_thing(thing):
+    not_class = isinstance(thing, types.ModuleType)
+
+    import_path = thing.__name__ if not_class else thing.__module__
+
+    return join(*import_path.split('.')[:2])
+
+
+def build_tree(root, packages):
+    for package in packages:
+        os.makedirs(join(root, dir_for_thing(package)), exist_ok=True)
+
+
+def ensure_data(prov):
+    """
+    Given that `prov`
+    """
+    if not prov.has_required_data() and hasattr(prov, 'obtain_data'):
+        val = prov.obtain_data()
+        assert val in {True, False}, prov  # ensure an explicit value
+
+        if not val:
+            logging.warning("Couldn't obtain data for %d", prov)
+
+
+def load_image_providers():
+    """
+    Loads image providing modules in submodules of `sections`.
+    Said image providers are declared in an `IMAGES` list variable on the
+    submodule of `sections`
+    """
+
+    logging.info('Loading packages')
+    packages = [
+        loader.find_module(name).load_module()
+        for loader, name, _ in pkgutil.walk_packages(['.'])
+        if name.startswith('sections.')
+    ]
+
+    top_level = [
+        package
+        for package in packages
+        if package.__name__.count('.') == 1
+        and basename(package.__file__) == '__init__.py'
+    ]
+
+    image_providers = [
+        (package,) + tuple(image_provider.split('.'))
+        for package in top_level
+        for image_provider in package.IMAGES
+    ]
+
+    logging.info('Loading image providers')
+    return [
+        getattr(
+            importlib.import_module(
+                '.' + submodule,
+                package.__package__
+            ),
+            classname
+        )
+        for package, submodule, classname in image_providers
+    ]
+
+
+def setup():
+    image_providers = load_image_providers()
+
+    logging.info('Building directories')
+    build_tree(CACHE, image_providers)
+    build_tree(join(HERE, 'output'), image_providers)
+
+    logging.info('Downloading requisite data')
+
+    data_dir = join(CACHE, 'aus_map')
+    os.makedirs(data_dir, exist_ok=True)
+    ensure_data(sections.aus_map.AusMap(data_dir))
+
+    image_providers = [
+        prov(join(CACHE, dir_for_thing(prov)))
+        for prov in image_providers
+    ]
+
+    # from concurrent.futures import ThreadPoolExecutor
+    # map = ThreadPoolExecutor(10).map
+    list(map(ensure_data, image_providers))
+
+    return image_providers
+
+from sections.shape import ShapeFileNotFoundException
+
+
+def build_images(image_providers):
+    logging.info('Rendering images')
+    for prov in image_providers:
+        logging.info('Rendering %s', prov)
+
+        try:
+            output_filename = join(
+                'output',
+                dir_for_thing(prov),
+                '{}.png'.format(prov.__module__.split('.')[-1])
+            )
+            if exists(output_filename):
+                continue
+            prov.build_image(output_filename)
+
+        except NotImplementedError:
+            logging.info("Can't render %s", prov.__name__)
+
+        except ShapeFileNotFoundException:
+            logging.info(
+                "Can't render %s; couldn't access required data",
+                prov.__name__
+            )
+
+
+def main():
+    image_providers = setup()
+
+    build_images(image_providers)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,47 @@
+from unittest.mock import patch
+from contextlib import contextmanager
+from collections import namedtuple
+from functools import wraps
+
+import requests
+import arcrest.compat
+from betamax import Betamax
+
+Res = namedtuple('Res', 'url,headers,read')
+
+
+def compat(func=None):
+    if func:
+        return compat_decorator(func)
+    else:
+        return compat_context()
+
+
+def compat_decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kw):
+        with compat_context():
+            return func(*args, **kw)
+    return wrapper
+
+
+def side_effect(req):
+    r = requests.get(
+        req.get_full_url(),
+        req.headers,
+        data=req.data
+    )
+
+    return Res(r.url, r.headers, lambda: r.content)
+
+
+@contextmanager
+def compat_context():
+    """
+    Patches in heavy-duty caching and requests-lib use
+    """
+
+    arcrest.compat.sess = requests.Session()
+    with patch('arcrest.compat.urllib2.urlopen', side_effect=side_effect):
+        with Betamax(arcrest.compat.sess).use_cassette('cache'):
+            yield
