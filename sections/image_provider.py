5aaf0bb90be2dfe0c226fdf64420307f52a5d47b
Dominic May <jack.thatch@gmail.com>
2015-07-11 00:40:56 +0800

    Say what isn't implemented on what

--- sections/image_provider.py
+++ sections/image_provider.py
@@ -1,15 +1,33 @@
+import inspect
+
+
+def not_implemented():
+    frame_info = inspect.currentframe().f_back
+    msg = ''
+
+    if 'self' in frame_info.f_locals:
+        self = frame_info.f_locals['self']
+        try:
+            msg += self.__name__ + '#'  # for static/class methods
+        except AttributeError:
+            msg += self.__class__.__name__ + '.'
+
+    msg += frame_info.f_code.co_name + '()'
+
+    return NotImplementedError(msg)
+
 
 class RequiresData:
     def __init__(self, data_dir):
         self.data_dir = data_dir
 
     def has_required_data(self):
-        raise NotImplementedError
+        raise not_implemented()
 
     def obtain_data(self):
-        raise NotImplementedError
+        raise not_implemented()
 
 
 class ImageProvider(RequiresData):
     def build_image(self, output_filename):
-        raise NotImplementedError
+        raise not_implemented()
