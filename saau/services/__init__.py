90da587382647879d18b5f6822aa0277cb0cdb4c
Dominic May <jack.thatch@gmail.com>
2015-08-02 03:16:33 +0800

    Actually properly index services

--- saau/services/__init__.py
+++ saau/services/__init__.py
@@ -1,17 +1,17 @@
+import re
 
+def build_name(serv):
+    return re.sub(
+        r'([A-Z][a-z])',
+        lambda m: '_' + m.group(0).lower(),
+        get_name(serv)
+    ).lower().strip('_')
 
 class Services:
     def inject(self, services):
-        self.services = services
+        self.services = {build_name(serv): serv for serv in services}
 
-    def services(self):
-        try:
-            return self._services
-        except AttributeError:
-            return self._load_services()
 
-    def _load_services(self):
-        self._services = []
 
     def __getattr__(self, name):
         try:
