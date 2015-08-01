9fd37c920333c88fefe919a4b0aa31c6abe08b07
Dominic May <jack.thatch@gmail.com>
2015-08-01 19:20:42 +0800

    Add caching to towns

--- saau/sections/towns.py
+++ saau/sections/towns.py
@@ -2,18 +2,15 @@ from functools import reduce
 
 import pandas
 
+from . import Singleton
 from .aus_map import AusMap
+from .image_provider import RequiresData
 from .shape import shape_from_zip
 
 
-def get_towns():
-    path = AusMap(None).data_dir_join('AUS_adm.zip')
-    shire_data = shape_from_zip(path, 'AUS_adm2')
 
-    return combine_towns({
-        record.attributes['NAME_2'].strip(): record
-        for record in shire_data.records()
-    })
+def get_towns():
+    return TownsData.instance().get_towns()
 
 
 class DummyRecord:
@@ -58,3 +55,35 @@ def combine_towns(towns):
         towns[town] = DummyRecord({'NAME_2': town}, geometry)
 
     return towns
+
+
+class TownsData(RequiresData, metaclass=Singleton):
+    def __init__(self, data_dir):
+        super().__init__(data_dir)
+
+    def has_cached(self):
+        return self.data_dir_exists('towns.pickle')
+
+    def load_cached(self):
+        with open(self.data_dir_join('towns.pickle'), 'rb') as fh:
+            return pickle.load(fh)
+
+    def cache(self, data):
+        with open(self.data_dir_join('towns.pickle'), 'wb') as fh:
+            pickle.dump(data, fh)
+        return data
+
+    def get_towns(self):
+        if self.has_cached():
+            return self.load_cached()
+
+        else:
+            path = AusMap.instance().data_dir_join('AUS_adm.zip')
+            shire_data = shape_from_zip(path, 'AUS_adm2')
+
+            val = combine_towns({
+                record.attributes['NAME_2'].strip(): record
+                for record in shire_data.records()
+            })
+
+        return self.cache(val)
