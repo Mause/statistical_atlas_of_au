8a79f36a1b529d060378238f0c3c673c60cb7c38
Dominic May <jack.thatch@gmail.com>
2015-08-01 17:59:11 +0800

    Implement bulk of median age

--- saau/sections/age/median.py
+++ saau/sections/age/median.py
@@ -1,8 +1,9 @@
-import json
+from matplotlib.cm import get_cmap
+import cartopy.crs as ccrs
 
-from ..towns import get_towns
+from ..towns import get_towns, TownsData
 from ..aus_map import get_map
-from ..abs import get_generic_data
+from ..abs import get_generic_data, collapse_concepts
 from ..image_provider import ImageProvider
 
 DATASETID = 'ABS_CENSUS2011_B02'
@@ -18,10 +19,54 @@ class MedianAgeImageProvider(ImageProvider):
             DATASETID,
             and_=[
                 'FREQUENCY.A',
-                'STATE.0',
                 'REGIONTYPE.SA3',
                 'MEASURE.MAGE'
+            ],
+            or_=[
+                'STATE.0',
+                'STATE.1',
+                'STATE.2',
+                'STATE.3',
+                'STATE.4',
+                'STATE.5',
+                'STATE.6',
+                'STATE.7',
+                'STATE.8',
+                'STATE.9'
             ]
         )
+        assert data['series']
 
         return self.save_json(FILENAME, data)
+
+    def build_image(self, _):
+        aus_map = get_map()
+        towns = get_towns()
+        colors = get_cmap('Purples')
+
+        age_data = self.load_json(FILENAME)
+        age_data = [
+            dict(
+                collapse_concepts(data_point['concepts']),
+                **data_point['observations'][0]
+            )
+            for data_point in age_data['series']
+        ]
+
+        region_lookup = TownsData.instance().sla_to_sla_name
+
+        __import__('ipdb').set_trace()
+
+        for data_point in age_data:
+            try:
+                town_name = region_lookup(data_point['REGION'])
+            except KeyError:
+                continue
+
+            aus_map.add_geometries(
+                [towns[town_name].geometry],
+                crs=ccrs.PlateCarree(),
+                color=colors(data_point['MAGE'])
+            )
+
+        return aus_map
