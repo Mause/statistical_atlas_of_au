f398887d3124f126cdf90c678136ef946cc40cfd
Dominic May <jack.thatch@gmail.com>
2015-08-02 18:13:17 +0800

    Headers for rest of transportation

--- saau/sections/transportation/railroads.py
+++ saau/sections/transportation/railroads.py
@@ -1,6 +1,18 @@
 from . import TransportationImageProvider
+from ..misc.header import render_header_to
 
 
 class RailroadImageProvider(TransportationImageProvider):
     path = 'railroad_paths.json'
     layers = ['Railways']
+
+    def build_image(self, _):
+        return render_header_to(
+            super().build_image(_),
+            19.5,
+            [
+                '<b>MAP OF</b>',
+                '<b>RAILROADS IN AUSTRALIA</b>',
+                '<i>Compiled using data from Geography Australia</i>'
+            ]
+        )
--- saau/sections/transportation/roads.py
+++ saau/sections/transportation/roads.py
@@ -1,6 +1,18 @@
 from . import TransportationImageProvider
+from ..misc.header import render_header_to
 
 
 class RoadImageProvider(TransportationImageProvider):
     path = 'road_paths.json'
     layers = ['All_Roads', 'Major_Road_Network']
+
+    def build_image(self, _):
+        return render_header_to(
+            super().build_image(_),
+            19.5,
+            [
+                '<b>MAP OF</b>',
+                '<b>MAJOR ROADS IN AUSTRALIA</b>',
+                '<i>Compiled using data from Geography Australia</i>'
+            ]
+        )
