e35d248ec268dc29f2520200cc3c752f6dff73e8
Dominic May <me@mause.me>
2017-07-22 18:03:42 +0800

    Disability templates

---
+++ saau/sections/disability/cognitive.py
@@ -0,0 +1,9 @@
+"""
+http://www.abs.gov.au/ausstats/abs@.nsf/mf/4433.0.55.003
+"""
+
+from ..image_provider import ImageProvider
+
+
+class CognitiveDisabilityImageProvider(ImageProvider):
+    pass
