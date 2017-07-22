fb0daadcec98a71e03ea53f92dce7a0950db4d79
Dominic May <me@mause.me>
2017-07-22 13:08:37 +0800

    Actually rewrite fonts to be a service

--- saau/services/fonts/__init__.py
+++ saau/services/fonts/__init__.py
@@ -1,10 +1,43 @@
-from os.path import expanduser
+from os.path import join, exists
 
 from matplotlib import font_manager
 
-hand_shop = font_manager.FontProperties(
-    fname=expanduser(
-        '~/Downloads/hand_shop_typography_c30/'
-        'Fontscafe_HandShopTypography-C30_demo.ttf'
-    )
-)
+from ...sections.image_provider import RequiresData
+from ...utils.download import get_binary
+from ...utils import unzip
+
+
+SERVICES = ['__init__.FontProvider']
+
+
+class FontProvider(RequiresData):
+    service_name = 'fonts'
+
+    FILENAME = 'Fontscafe_HandShopTypography-C30_demo'
+
+    def path(self):
+        return self.data_dir_join(
+            join(
+                self.FILENAME,
+                self.FILENAME + ".ttf"
+            )
+        )
+
+    def has_required_data(self):
+        return exists(self.path())
+
+    def obtain_data(self):
+        filename = self.data_dir_join(self.FILENAME + ".zip")
+        res = get_binary(
+            'http://dl.dafont.com/dl/?f=hand_shop_typography_c30',
+            filename
+        )
+        if not res:
+            return res
+
+        unzip(filename)
+
+        return self.has_required_data()
+
+    def get_font(self):
+        return font_manager.FontProperties(fname=self.path())
