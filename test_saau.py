dee03477b183d6c76a11d92ddb065b678ff82d99
Elliana <me@mause.me>
2020-12-29 19:40:19 +0800

    Check actual length

--- test_saau.py
+++ test_saau.py
@@ -2,8 +2,8 @@ from saau.loading import load_image_providers, load_service_providers
 
 
 def test_load_service_providers():
-    assert load_service_providers(None)
+    assert list(load_service_providers(None))
 
 
 def test_load_image_providers():
-    assert load_image_providers(None)
+    assert list(load_image_providers(None))
