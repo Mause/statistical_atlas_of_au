2d13ef855e5e468ce4365e30e9d75fcd56832c04
Elliana <me@mause.me>
2020-12-29 20:35:50 +0800

    Extend test

--- test_saau.py
+++ test_saau.py
@@ -1,8 +1,16 @@
 from saau.loading import load_image_providers, load_service_providers
+from saau.__main__ import initialize_providers, Services
 
 
 def test_load_service_providers():
-    assert list(load_service_providers(None))
+    services = list(load_service_providers(None))
+    services_container = Services()
+    assert services
+    services = initialize_providers(services, services_container)
+    assert services
+
+    services_container.inject(services)
+    assert services_container.fonts.get_font()
 
 
 def test_load_image_providers():
